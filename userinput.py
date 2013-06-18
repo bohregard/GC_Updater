bc4065cef37357ed1dc39a18455baa426ebc4441
bohregard <aw.todd89@gmail.com>
2013-06-18 17:03:31 -0400

    Userinput.py update

--- userinput.py
+++ userinput.py
@@ -1,4 +1,4 @@
-import webParse, downloads, time, compare
+import webParse, downloads, time, compare, SilentInstall
 
 print "Choose an option:\n1: Check for updates\n2: Download and install updates\n3: Check and download updates (no install)"
 
@@ -23,7 +23,22 @@ while x != 'x':
         
         break
     elif x == '2':
-        print "Choice 2"
+        print "Checking versions from web...\n"
+        time.sleep(.5)
+        adobeupdate = webParse.AdobeUpdater()
+        (windows, firefox) = adobeupdate.adobe_version()
+
+        javaupdate = webParse.JavaUpdater()
+        (java) = javaupdate.java_check()
+
+        print "Comparing versions...\n"
+        time.sleep(.5)
+        textversion = downloads.Downloads()
+        (windows_txt,firefox_txt,java_txt) = textversion.version_print()
+
+        compare.Compare(windows,windows_txt,firefox,firefox_txt,java,java_txt)
+
+        
         break
     elif x == '3':
         print "Choice 3"
