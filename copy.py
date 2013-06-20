aef5347b7d4c6f7b432085b5f1ad8d7148e9c6b6
bohregard <aw.todd89@gmail.com>
2013-06-20 10:39:04 -0400

    Cleanup

--- copy.py
+++ copy.py
@@ -1,17 +1,11 @@
 import os,shutil
 
-
-
 class CopyFiles:
     def __init__(self,cpfile):
-##        if __name__ == "__main__":
-##            config = {}
-##            execfile("config.conf",config)
-## 
-##            dropbox = config["dropbox"]
-##            tools = config["techtool"]
         print "\nCopying "+cpfile+"...\n"
+        #Value needs to be edited.
         dropbox = "C:\Dropbox"
+        #End edit
         tools = "Technician Toolbox\Service Bundles\TuneUp - Vispy"
         shutil.copy(cpfile,dropbox+'\\'+tools)
-        print "Copying of"+cpfile+"completed\n"
+        print "Copying of "+cpfile+"completed\n"
