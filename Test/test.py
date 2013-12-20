34eb3055fc4a7578afa0ad4e43821721a661b044
juoni <terrenceandrews21@gmail.com>
2013-12-19 17:22:55 -0800

    'removed'

--- Test/test.py
+++
@@ -1,8 +0,0 @@
-import urllib, os, sys,time
-
-def dlProgress(count, blockSize, totalSize):
-    percent = int(count*blockSize*100/totalSize)
-    sys.stdout.write("\r...%d%%" % percent)
-    sys.stdout.flush()
-
-urllib.urlretrieve('http://javadl.sun.com/webapps/download/AutoDL?BundleId=78825', 'java.exe', reporthook=dlProgress)
